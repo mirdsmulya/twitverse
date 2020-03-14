ca3bbcbc186ef9dc1269cd240a1a5a93e91918db
mirdan <syahidmirdan@gmal.com>
2020-03-14 16:31:08 +0700

    Add navigation menu to sidebar

---
+++ src/common/SideBarLeft.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Link, IndexLink } from 'react-router';
+
+const SidebarLeft = () => {
+    let buttonText = "Sign Out";
+    let username =  sessionStorage.getItem("currentUserLogin")
+    
+    
+    return (
+ 
+        <div className="fixed-sidebar ">
+				<div className="sticky ">
+				<div className="header-logo ">
+                <div className=" ">
+                <button className="" onClick={() => {sessionStorage.removeItem("currentUserLogin"); this.props.history.push('/login');}}>{buttonText}</button>
+                                    <p className="sidebar-text">{username}</p>
+                </div>  
+
+                </div>
+                <div className="sidebar ">
+                            
+                    <IndexLink to="/"  activeClassName="active" className="sidebar-text" > 
+                    <div className="sidebar-text-box">My Profile</div>
+                    </IndexLink>
+                    
+                    <Link to="/" activeClassName="active" className="sidebar-text" > 
+                    <div className="sidebar-text-box">Friends</div>
+                    </Link>
+
+                    <Link to="/" activeClassName="active" className="sidebar-text" > 
+                    <div className="sidebar-text-box">Albums</div>
+                    </Link>
+     
+                            
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default SidebarLeft;
