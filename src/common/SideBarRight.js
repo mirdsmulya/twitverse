5ca52e969488d148a9d4a4534f9797020e3d4413
mirdan <syahidmirdan@gmal.com>
2020-03-14 16:30:02 +0700

    Initial code for right sidebar

---
+++ src/common/SideBarRight.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+
+const SideBarRigth = ({}) => {
+    return (
+        <div className='sidebar-right border'>
+			<div className=''>
+			<h1>Hello from the other side!</h1> 
+                
+			</div>
+				
+        </div>
+    );
+};
+export default SideBarRigth;
