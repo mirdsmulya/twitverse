70288fae010b56842d2d6d4090ffed1d77a47956
mirdan <syahidmirdan@gmal.com>
2020-03-14 23:33:09 +0700

    Add mapStateToProps and mapDispatcToProps into ListUserPage

--- src/component/ListUserPage.js
+++ src/component/ListUserPage.js
@@ -2,12 +2,27 @@
 
 import React from 'react';
 import UserBox from '../common/UserBox';
-
-import Data from '../../dataUser.json';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as dataAction from '../actions/dataAction';
 
 class ListUserPage extends React.Component {
+    constructor(props, context){
+        super(props, context);
+        this.state = {
+            userData: this.props.datas
+        } ;
+
+    }
+
+    componentWillReceiveProps() {
+        debugger;
+        this.setState({userData: this.props.datas})
+    }
+
     render() {
-        let users = Data.data;
+        let users = this.props.datas;
+        debugger;
         return(
             <div className="line-menu">
                 Hellow World!
@@ -18,4 +33,19 @@ class ListUserPage extends React.Component {
         )
     }
 }
-export default ListUserPage;
+
+export function mapStateToProps(state, ownProps) {
+    debugger;
+    return {
+        datas: state.datas
+    }
+}
+
+export function mapDispatchToProps(dispatch) {
+    return {
+        action: bindActionCreators(dataAction, dispatch)
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListUserPage);
