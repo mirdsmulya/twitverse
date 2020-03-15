91881815dd863041cacc81fce395b45b6b17e0e7
mirdan <syahidmirdan@gmal.com>
2020-03-15 16:27:27 +0700

    Add route to profile and login page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './component/App';
-import ListUserPage from './component/ListUserPage'
+import ListUserPage from './component/ListUserPage';
+import ProfilePage from './component/ProfilePage';
+import LoginPage from './component/LoginPage';
 
 
 export default (
 
     <Route path="/" component={App} >
 		<IndexRoute component={ListUserPage} />
+		<Route path="profile/:id" component={ProfilePage} />
+		<Route path="profile" component={ProfilePage} />
+		<Route path="login" component={LoginPage} />
 
 	</Route>
       
